d74df7ff9b3df1c3228245526deaef980f8df81a
anporus <anporus@gmail.com>
2017-08-16 22:07:05 +0700

    Create anporusapi.js

---
+++ anporusapi.js
@@ -0,0 +1,14 @@
+function apiplayer(link){
+	if(link.match(/(phimbathu.com|bilutv.com)/ig)){
+		var apiplayer = "//player.trunguit.net/play?url=";
+	}else if(link.match(/(phimmoi.net)/ig)){
+		var apiplayer = "//player.trunguit.net/play?url=";
+	}else if(link.match(/(youtube.com)/ig)){
+		var apiplayer = "http://videoapi.io/embed/?hash=vs9897u492vs97467s095517t26458u1&amp;url=";
+	}else{
+		//var apiplayer = "//player.trunguit.net/play?url=";
+		var apiplayer = "http://vntool.net/code/player.php?url=";
+		//var apiplayer = "//megavn.net/player/iframe.php?film_url=";
+	}
+	return apiplayer+link;
+}
