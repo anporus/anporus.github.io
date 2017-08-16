1a56fe822c31d39e892f8854f5a55582a2239a02
anporus <anporus@gmail.com>
2017-08-17 00:54:17 +0700

    Update anporusapi.js

--- anporusapi.js
+++ anporusapi.js
@@ -1,8 +1,7 @@
-var block = ["daophim.net"];
+var block = ["phimmoi.net"];
 var arrBlock = block.length;
 for(var i=0;i<arrBlock;i++){
     if(window.location.href.indexOf(block[i])>-1){
-        //alert(block[i]+" block");
         window.top.location.href="http://www.daophim.net";
     }else{
 		function apiplayer(link){
