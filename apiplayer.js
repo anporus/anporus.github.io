b8a0f25f4592021f9ca7dc219383171db3bde293
anporus <anporus@gmail.com>
2017-08-20 01:25:30 +0700

    Update apiplayer.js

--- apiplayer.js
+++ apiplayer.js
@@ -52,9 +52,9 @@ var msg=document.getElementsByName("info");for(var i=0;i<msg.length;i++){
 
     //$(".tysuncontent:eq("+i+")").html(newtext[2]);
     document.getElementsByName("tysuncontent")[i].innerHTML = newtext[2];
+
     //if(newtext[3]){
     //$(".tysuntags:eq("+i+")").html(newtext[3]);
-    //document.getElementsByName('tysuntags').innerHTML = newtext[3];
     //document.getElementsByName("tysuntags")[i].innerHTML = newtext[3];
     //}
 
