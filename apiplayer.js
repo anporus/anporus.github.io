78651d6c1cd1c360b1ee654088b5d27aa5601868
anporus <anporus@gmail.com>
2017-08-20 01:17:09 +0700

    Update apiplayer.js

--- apiplayer.js
+++ apiplayer.js
@@ -47,19 +47,25 @@ var msg=document.getElementsByName("info");for(var i=0;i<msg.length;i++){
     var oldtext = document.getElementsByName("info")[i].innerHTML;
     var newtext = oldtext.replace(/(^|[\n ])([\w]+?:\/\/[^ ,\"\n\r\t<]*)/ig, "$2").split('\n\[+\]');
     var thumbnew = newtext[1].replace(/s1600/ig, 's200').replace(/[0-9]+.bp.blogspot.com/,"lh4.googleusercontent.com").replace(/img src/ig, 'img class="thumb-info" src');
-    var name
     //$(".tysunimg:eq("+i+")").html(thumbnew);
-    document.getElementsByName('tysunimg').innerHTML = '' + thumbnew + '';
+    document.getElementsByName("tysunimg")[i].innerHTML = '' + thumbnew + '';
+
     //$(".tysuncontent:eq("+i+")").html(newtext[2]);
-    document.getElementsByName('tysuncontent').innerHTML = '' + newtext[2] + '';
+    document.getElementsByName("tysuncontent")[i].innerHTML = '' + newtext[2] + '';
+
     //$(".tysuntags:eq("+i+")").html(newtext[3]);
-    document.getElementsByName('tysuntags').innerHTML = '' + newtext[3] + '';
+    //document.getElementsByName('tysuntags').innerHTML = '' + newtext[3] + '';
+    //document.getElementsByName("tysuntags")[i].innerHTML = '' + newtext[3] + '';
+
     //$(".tysunqg:eq("+i+")").html(quocgia);
-    document.getElementsByName('tysunqg').innerHTML = '' + quocgia + '';
+    document.getElementsByName("tysunqg")[i].innerHTML = '' + quocgia + '';
+
     //$(".tysuntl:eq("+i+")").html(theloai);
-    document.getElementsByName('tysuntl').innerHTML = '' + theloai + '';
+    document.getElementsByName("tysuntl")[i].innerHTML = '' + theloai + '';
+
     //$(".tysunnxb:eq("+i+")").html(document.title.split(' - ')[1].replace(/(.+?)\(([0-9]+)\)/gi,'$2'));
-    document.getElementsByName('tysunnxb').innerHTML = '' + namxb + '';
+    document.getElementsByName("tysunnxb")[i].innerHTML = '' + namxb + '';
+
     status = document.getElementsByName('status')[0].innerHTML;
     document.getElementsByName('statusnew')[0].innerHTML = '' + status + '';
 }
